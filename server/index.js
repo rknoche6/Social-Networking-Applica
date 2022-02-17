98221f544c912ef038373ffde6b4879a54c79763
rknoche6 <rknoche@purdue.edu>
2022-02-17 15:28:12 +0100

    added connection to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const pool = require("./db");
 app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
