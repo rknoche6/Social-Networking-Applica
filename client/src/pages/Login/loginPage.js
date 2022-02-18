f7f40dbcbc89cfff2fe96e314df48d8664ed8c83
rknoche6 <rknoche@purdue.edu>
2022-02-18 19:48:25 +0100

    updated loginpage

--- client/src/pages/Login/loginPage.js
+++ client/src/pages/Login/loginPage.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./../HomePage/homePage";
 
 const LogInPage = () => {
   return (
-    <div className="login-container">
-      <h1 className="text-center mt-5">Log in</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" value={"email"} />
-        <input type="text" className="form-control" value={"password"} />
-        <button className="btn btn-success">Log in</button>
-      </form>
-    </div>
+    <Router>
+      <div className="login-container">
+        <h1 className="text-center mt-5">Log in</h1>
+        <form className="d-flex mt-5">
+          <input type="text" className="form-control" value={"email"} />
+          <input type="text" className="form-control" value={"password"} />
+          <Link to={"/tweets"} className="nav-link btn btn-success">
+            Log in
+          </Link>
+        </form>
+        <Switch>
+          <Route path="/tweets" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
-
-export default LogInPage;
