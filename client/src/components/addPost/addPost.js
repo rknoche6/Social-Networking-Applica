c5e7416ccfcf361fa95f1d70ee48f79e0f5a815d
rknoche6 <rknoche@purdue.edu>
2022-02-19 14:52:40 +0100

    connected add post to database

--- client/src/components/addPost/addPost.js
+++ client/src/components/addPost/addPost.js
@@ -2,10 +2,24 @@ import React, { useState } from "react";
 
 const AddPost = () => {
   const [description, setDescription] = useState("");
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      window.location = "/";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <div className="addPost-Container">
       <h1>Add Post</h1>
-      <form>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           id="add-post"
