a6e039719d7146de28d27001ded215091561e508
rknoche6 <rknoche@purdue.edu>
2022-02-20 19:32:29 +0100

    updated routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./components/HomePage/homePage";
 import LogInPage from "./pages/Login/loginPage";
+import PrimaryPage from "./pages/primary/primaryPage";
+import SignUpPage from "./pages/signUp/signUpPage";
+import HomePage from "./pages/Homepage/homePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" component={PrimaryPage} exact />
         <Route path="/login" component={LogInPage} exact />
-        <Route path="/" component={HomePage} exact />
+        <Route path="/signup" component={SignUpPage} exact />
+        <Route path="/home" component={HomePage} exact />
         <Route component={Error} />
       </Switch>
     </BrowserRouter>
--- client/src/pages/Login/loginPage.js
+++ client/src/pages/Login/loginPage.js
@@ -1,19 +1,20 @@
 import React, { useImperativeHandle } from "react";
 import { Link } from "react-router-dom";
 
+
 const LogInPage = () => {
-  return (
-    <div className="login-container">
-      <h1 className="text-center mt-5">Log in</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" value={"email"} />
-        <input type="text" className="form-control" value={"password"} />
-        <Link to="/home">
-          <button className="btn btn-success">Log in</button>
-        </Link>
-      </form>
-    </div>
-  );
-};
+    return (
+      <div className="login-container">
+        <h1 className="text-center mt-5">Log in</h1>
+        <form className="d-flex mt-5">
+          <input type="text" className="form-control" value={"email"} />
+          <input type="text" className="form-control" value={"password"} />
+          <Link to="/home">
+            <button className="btn btn-success">Log in</button>
+          </Link>
+        </form>
+      </div>
+    );
+  };
 
 export default LogInPage;
