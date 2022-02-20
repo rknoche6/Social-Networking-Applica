89af8f2f3d7187a7e241d453ae449154241e0b26
rknoche6 <rknoche@purdue.edu>
2022-02-20 16:16:42 +0100

    added deleting a post functionality

--- client/src/pages/Homepage/homePage.js
+++ client/src/pages/Homepage/homePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import AddPost from "../../components/addPost/addPost";
 import AllPosts from "../../components/feed/feed";
 
-
 const HomePage = () => {
   return (
     <div id="homepage-container">
