015ed6ed25377eaa12adcb4e2686950cc69896cc
rknoche6 <rknoche@purdue.edu>
2022-02-21 16:39:05 +0100

    added sql file

---
+++ server/db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE facebook;
+CREATE DATABASE facebook_test;
+
+CREATE TABLE posts(
+  post_id SERIAL PRIMARY KEY,
+  description VARCHAR(500)
+);
+
+CREATE TABLE Users
+(
+  user_id SERIAL Primary Key,
+  email varchar(255) ,
+  username varchar(255) ,
+  password varchar(255),
+  UNIQUE (email),
+  UNIQUE(username)
+);
+
+ALTER TABLE posts
+ADD COLUMN user_fk_id INTEGER;
+
+ALTER TABLE posts
+ADD CONSTRAINT constraint_fk FOREIGN KEY (user_fk_id) REFERENCES Users
+(user_id)
+ON
+DELETE CASCADE;
